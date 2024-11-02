9194c608b34e574f1ee5fa122d9fb91884eb48e7
Neskazu <erikfil1995@gmail.com>
2024-11-02 09:22:26 +0600

    Init

--- CityInfo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CityInfo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
