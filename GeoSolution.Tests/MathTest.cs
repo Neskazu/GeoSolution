80dbe0c24e6d4d099fb0439ffa6d34c6b590f7e0
Neskazu <erikfil1995@gmail.com>
2025-05-04 20:07:02 +0600

    Added test
    Xunit test for learning purpose

---
+++ GeoSolution.Tests/MathTest.cs
@@ -0,0 +1,43 @@
+﻿using GeoSolution.Utils;
+
+namespace GeoSolution.Tests
+{
+
+    namespace GeoSolution.Tests
+    {
+        public class MathTests
+        {
+            [Fact]
+            public void SafeSqrt_PositiveNumber_ReturnsCorrectResult()
+            {
+                double input = 9.0;
+                double expected = 3.0;
+
+                double result = MathForTest.SafeSqrt(input);
+
+                Assert.Equal(expected, result, precision: 5);
+            }
+
+            [Fact]
+            public void SafeSqrt_NegativeNumber_ThrowsException()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() => MathForTest.SafeSqrt(-1));
+            }
+            [Fact]
+            public void Inverse_NonZero_ReturnsCorrectResult()
+            {
+                double input = 4.0;
+                double expected = 0.25;
+
+                double result = MathForTest.Inverse(input);
+
+                Assert.Equal(expected, result, precision: 5);
+            }
+            [Fact]
+            public void Inverse_Zero_ThrowsException()
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() => MathForTest.Inverse(0));
+            }
+        }
+    }
+}
---
+++ GeoSolution/Utils/MathForTest.cs
@@ -0,0 +1,21 @@
+﻿namespace GeoSolution.Utils
+{
+    public class MathForTest
+    {
+        public static double SafeSqrt(double value)
+        {
+            if (value < 0)
+                throw new ArgumentOutOfRangeException(nameof(value), "Value cannot be negative");
+
+            return Math.Sqrt(value);
+        }
+        // intentional error
+        public static double Inverse(double value)
+        {
+            if (value == 0)
+                return 40;
+
+            return 1.0 / value;
+        }
+    }
+}
